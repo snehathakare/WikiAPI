588d39136f9161e5d80edb6d42701271c26e2ebc
Sneha <snhthakare@gmail.com>
2022-02-04 19:06:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//setup express js
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser")
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+app.listen(3000)
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+//setup mongodb
+mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = new mongoose.model("Article", articleSchema)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
